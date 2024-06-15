4b9a28e2ad3ca89d1d51b805bf99c19398cb0bb5
Elliott Minns <elliott.minns@pm.me>
2024-06-15 14:35:59 -0500

    chore: refactor commit command to use exec for git commit and update dependencies
    Refactored the commit command to use exec.Command for git commit instead of go-git library to be able to edit messages.
    Updated go.mod to include new dependencies and updated existing ones to their latest versions.

--- cmd/commit.go
+++ cmd/commit.go
@@ -6,11 +6,13 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 
-	"github.com/dreamsofcode-io/gent/internal/message"
 	"github.com/go-git/go-git/v5"
 	"github.com/spf13/cobra"
+
+	"github.com/dreamsofcode-io/gent/internal/message"
 )
 
 // commitCmd represents the commit command
@@ -24,15 +26,17 @@ var commitCmd = &cobra.Command{
 			return fmt.Errorf("failed to get current directory: %w", err)
 		}
 
-		r, err := git.PlainOpen(directory)
+		_, err = git.PlainOpen(directory)
 		if err != nil {
 			return fmt.Errorf("failed to open repository: %w", err)
 		}
 
-		w, err := r.Worktree()
-		if err != nil {
-			return fmt.Errorf("failed to get worktree: %w", err)
-		}
+		/*
+			w, err := r.Worktree()
+			if err != nil {
+				return fmt.Errorf("failed to get worktree: %w", err)
+			}
+		*/
 
 		msg, err := message.Generate(cmd.Context())
 		if err != nil {
@@ -40,7 +44,9 @@ var commitCmd = &cobra.Command{
 		}
 
 		fmt.Printf("Generated Commit Message:\n\n")
+
 		fmt.Println(msg)
+
 		fmt.Printf("\nProceed with commit message? (Y/n): ")
 
 		var confirmation string
@@ -55,17 +61,25 @@ var commitCmd = &cobra.Command{
 			return nil
 		}
 
-		commit, err := w.Commit(msg, &git.CommitOptions{})
-		if err != nil {
-			return fmt.Errorf("failed to commit: %w", err)
-		}
+		/*
+			commit, err := w.Commit(msg, &git.CommitOptions{})
+			if err != nil {
+				return fmt.Errorf("failed to commit: %w", err)
+			}
 
-		obj, err := r.CommitObject(commit)
-		if err != nil {
-			return fmt.Errorf("failed to get commit obj: %w", err)
-		}
+			obj, err := r.CommitObject(commit)
+			if err != nil {
+				return fmt.Errorf("failed to get commit obj: %w", err)
+			}
 
-		fmt.Println(obj)
+			fmt.Println(obj)
+		*/
+
+		err = exec.Command("git", "commit", "-em", msg).Run()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "failed to start proc:", err)
+			return err
+		}
 
 		return nil
 	},
